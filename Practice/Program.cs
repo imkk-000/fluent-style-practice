2fd5ef0c1e39e11730e11fadb875ae7b702fc5ea
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-26 16:19:08 +0700

    Add: initial dotnet files and Program without fluent style

---
+++ Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            var numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var sum = 0;
+            var typeOfSum = "";
+
+            // find sum of numbers array
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            // find type sum of numbers
+            if (sum % 2 == 1) {
+                typeOfSum = "Odd";
+            } else {
+                typeOfSum = "Even";
+            }
+
+            // print the result
+            Console.WriteLine("sum of numbers is {0}, and type of sum is {1}.", sum, typeOfSum);
+        }
+    }
+}
