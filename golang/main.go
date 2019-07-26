968341f7f92be96a3237c6b1440bb3af73b97184
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-26 18:04:39 +0700

    Edit: test for multiple test data

--- golang/main.go
+++ golang/main.go
@@ -3,8 +3,14 @@ package main
 import "practice/myfluent"
 
 func main() {
+	TestExampleWith([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	TestExampleWith([]int{1})
+	TestExampleWith([]int{2})
+}
+
+func TestExampleWith(testData []int) {
 	m := &myfluent.MyFluentStruct{}
-	m.SetArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	m.SetArray(testData)
 	m.CalculateSum()
 	m.GetTypeOfSum()
 	m.PrintResult()
