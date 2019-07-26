b6d950b42670dbcb3329f9e5d3acbbaf24268d63
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-26 16:56:37 +0700

    Edit: group all fluent method

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,11 +6,18 @@ namespace Practice
     {
         static void Main(string[] args)
         {
+            TestExampleWith(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            TestExampleWith(new int[] {1});
+            TestExampleWith(new int[] {2});
+        }
+
+        static void TestExampleWith(int[] testData) {
             new MyFluentClass()
-                .SetArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+                .SetArray(testData)
                 .CalculateSum()
                 .GetTypeOfSum()
-                .PrintResult();
+                .PrintResult()
+                ;
         }
     }
 }
