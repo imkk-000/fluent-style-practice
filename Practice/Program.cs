d69c5163431156bb02e4a80d036337239b52841a
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-26 16:45:45 +0700

    Edit: transform class Program to fluent style

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,26 +6,11 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            // declare variables
-            var numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            var sum = 0;
-            var typeOfSum = "";
-
-            // find sum of numbers array
-            foreach (var number in numbers)
-            {
-                sum += number;
-            }
-
-            // find type sum of numbers
-            if (sum % 2 == 1) {
-                typeOfSum = "Odd";
-            } else {
-                typeOfSum = "Even";
-            }
-
-            // print the result
-            Console.WriteLine("sum of numbers is {0}, and type of sum is {1}.", sum, typeOfSum);
+            new MyFluentClass()
+                .SetArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+                .CalculateSum()
+                .GetTypeOfSum()
+                .PrintResult();
         }
     }
 }
